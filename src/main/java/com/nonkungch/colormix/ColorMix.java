5a2daa7dad04d6e2a22eee248b92e4903385eadb
NonKungCh. <116896756+NonKun1122@users.noreply.github.com>
2026-02-17 17:18:56 +0700

    Create ColorMix.java

---
+++ src/main/java/com/nonkungch/colormix/ColorMix.java
@@ -0,0 +1,14 @@
+package com.nonkungch.colormix;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ColorMix.MOD_ID)
+public class ColorMix {
+    public static final String MOD_ID = "colormix";
+
+    public ColorMix(IEventBus modEventBus) {
+        // Register mod initialization here
+        // Example: modEventBus.addListener(this::commonSetup);
+    }
+}
